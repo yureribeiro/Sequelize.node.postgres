0024a644789cdb0155d49ae43940edea4523ba7c
Yure Ribeiro <yureribeirocontato@gmail.com>
2023-03-08 21:20:05 -0300

    add caps and spaceships

--- config/associations.js
+++ config/associations.js
@@ -1,5 +1,21 @@
-const Planet = require("../models/Planet");
+const Planet = require("../models/Planet")
 const Satelite = require("../models/Satelites")
+const Cap = require('../models/Cap')
+const Spaceship = require("../models/Spaceship")
 
-Planet.hasOne(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Planet.hasMany(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" })
 Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" })
+
+Cap.belongsToMany(Spaceship, {
+  foreingKey: 'capId',
+  through: 'capSpaceship',
+  as: 'spaceships',
+})
+
+Spaceship.belongsToMany(Cap, {
+  foreingKey: 'spaceshipId',
+  through: 'capSpaceship',
+  as: 'caps',
+})
+
+module.exports = { Planet, Satelite }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const PlanetController = require("../Controllers/PlanetController");
 const SateliteController = require('../controllers/SateliteController')
+const CapController = require('../controllers/CapController');
+const SpaceshipController = require("../controllers/SpaceshipController");
 
 // Rotas de Planets
 routes.post("/planets", PlanetController.store);
@@ -10,8 +12,16 @@ routes.get("/planets", PlanetController.index);
 routes.put("/planets/:id", PlanetController.put);
 routes.delete("/planets/:id", PlanetController.delete);
 
+//rotas satelites
 routes.post("/planet/:planetId/satelites", SateliteController.store);
-routes.post("/planet/:planetId/satelites", SateliteController.index);
+routes.get("/planet/:planetId/satelites", SateliteController.index);
 
+//rotas capitao
+routes.post("/cap", CapController.store)
+routes.get("/cap", CapController.index)
+
+//rotas naves
+routes.post("/caps/:capId/spaceships", SpaceshipController.store)
+routes.get("/caps/:capId/spaceships", SpaceshipController.index)
 
 module.exports = routes;
